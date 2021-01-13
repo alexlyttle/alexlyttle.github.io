5697f7e2ac404139eadf92d68e7bfdae3ec5c43f
alexlyttle <a.j.lyttle@outlook.com>
2021-01-13 13:43:53 +0000

    Added academicons and tidied code

--- assets/jsx/main.jsx
+++ assets/jsx/main.jsx
@@ -8,14 +8,11 @@ class LatestCommitComponent extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            created: "",
-            updated: ""
+            status: "",
         };
     }
   
     componentDidMount() {
-        // Replace this with your own Github gist
-        // https://api.github.com/gists/:sha
         // The 'sha' is the hash key for the GitHub Gist
         fetch(
             "https://api.github.com/gists/" + this.props.sha
@@ -25,9 +22,13 @@ class LatestCommitComponent extends React.Component {
                 let options = { day: 'numeric', month: 'long', year: 'numeric' };  // Options for date format
                 var created = new Date(json.created_at).toLocaleString('default', options);
                 var updated = new Date(json.updated_at).toLocaleString('default', options);
+                var status = "Created: " + created + " \u2022 " + "Updated: " + updated
+                // If created and updated are the same then only show created date
+                if (created == updated) {
+                    status = created
+                }
                 this.setState({
-                    created: created,
-                    updated: updated
+                    status: status
                 });
             });
         }).catch(error => {
@@ -36,21 +37,11 @@ class LatestCommitComponent extends React.Component {
     }
   
     render() {
-        // If created and updated dates are the same, only display the created date
-        if (this.state.created == this.state.updated) {
-            return (
-                <div>
-                    {this.state.created}
-                </div>
-            );
-        } else {
-            return (
-                <div>
-                    Created: {this.state.created} &nbsp;&bull;&nbsp; Updated: {this.state.updated}
-                </div>
-            );
-        }
+        return (
+            this.state.status
+        )
     }
 }
 
 ReactDOM.render(<LatestCommitComponent sha="92b6862286990967cfa9deb5e91df9a3" />, document.getElementById("dream-analysis-date"));
+
